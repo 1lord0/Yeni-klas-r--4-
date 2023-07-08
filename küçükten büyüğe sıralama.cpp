878898f4ffda55e3f6e195e619da55584a1a5874
Akif Eren <125217594+1lord0@users.noreply.github.com>
2023-07-08 23:50:39 +0300

    Add files via upload

---
+++ küçükten büyüğe sıralama.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void SortIntArr(int arr[], int size) {
+    int i, j, temp;
+    
+    for (i = 0; i < size-1; i++) {
+        for (j = 0; j < size-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int size, i;
+    
+    printf("Dizi boyutunu girin: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    
+    printf("Dizinin elemanlarýný girin:\n");
+    for (i = 0; i < size; i++) {
+        printf("Eleman %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    
+    SortIntArr(arr, size);
+    
+    printf("\nSýralanmýþ dizi: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
+
